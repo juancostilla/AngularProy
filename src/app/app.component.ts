e0c6ec273a7ee5dedf0cf572f51fa503bbac1383
juan.costilla <juan.costilla@theinfotechpartners.com>
2023-04-09 02:41:02 +0100

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularProy';
+  http = inject(HttpClient);
+  products: Product[] = [];
+
+  changeTitle(){
+    this.title = 'New Value';
+  }
+
+  ngOnInit() {
+    this.http.get('https://api.escuelajs.co/api/v1/products')
+      .subscribe((data: any) => {
+        this.products = data;
+      })
+  }
 }
+
